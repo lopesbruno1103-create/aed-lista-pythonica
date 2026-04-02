88266a84575848a31896be9d942dac14ea581513
lopesbruno1103-create <lopesbruno1103@gmail.com>
2026-04-01 21:27:09 -0300

    Rename function to mensagem_simples and simplify
    Renamed function and simplified its implementation.

--- src/exercicio_02.py
+++ src/exercicio_02.py
@@ -1,11 +1,2 @@
-def simple_message(message: str) -> str:
-    """
-    Retorna a mesma mensagem recebida.
-
-    Args:
-        message (str): mensagem de entrada
-
-    Returns:
-        str: a mesma mensagem
-    """
-    pass
+def mensagem_simples(texto):
+    return texto
