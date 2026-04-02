d5fcf65dc5f02a19501ba2c01edfe48b0286013b
lopesbruno1103-create <lopesbruno1103@gmail.com>
2026-04-01 22:03:43 -0300

    Rename and implement odd_numbers function
    Refactor odd_numbers function to numeros_impares and implement logic to return odd numbers.

--- src/exercicio_16.py
+++ src/exercicio_16.py
@@ -1,11 +1,6 @@
-def odd_numbers(n: int) -> list[int]:
-    """
-    Retorna os números ímpares de 1 até n.
-
-    Args:
-        n (int): limite superior
-
-    Returns:
-        list[int]: lista de números ímpares
-    """
-    pass
+def numeros_impares(n):
+    lista = []
+    for i in range(1, n+1):
+        if i % 2!0:
+        lista.append(i)
+    return lista
