991c1031632075d397e2630bfe748004a427fee0
lopesbruno1103-create <lopesbruno1103@gmail.com>
2026-04-01 21:58:28 -0300

    Refactor number generation function
    Updated function to generate a list of numbers from 1 to n.

--- src/exercicio_14.py
+++ src/exercicio_14.py
@@ -1,11 +1,5 @@
-def generate_numbers(n: int) -> list[int]:
-    """
-    Gera uma lista de números de 1 até n.
-
-    Args:
-        n (int): limite superior
-
-    Returns:
-        list[int]: lista [1, 2, ..., n]
-    """
-    pass
+def gerar_numeros(n):
+    lista = []
+    for i in range (1, n+1):
+        lista.append
+    return lista
